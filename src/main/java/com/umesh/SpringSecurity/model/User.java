2c58e447d54834fab18556202af2b43180357175
umesh <umeshwalagedara@gmail.com>
2017-08-21 01:01:20 +0530

    Chages to User class

---
+++ src/main/java/com/umesh/SpringSecurity/model/User.java
@@ -0,0 +1,15 @@
+package com.umesh.SpringSecurity.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by umesh on 8/20/17.
+ */
+
+@Entity
+@Table(name = "user")
+public class User {
+
+
+}
